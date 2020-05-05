2efcebccb65a5a2e1e83d9814c7966d7a7afb0e6
Kerby-vlad <53996780+Kerby-vlad@users.noreply.github.com>
2020-05-05 14:34:48 +0300

    Мобилка v.2 Неработающая тема

--- scripts/theme-other.js
+++ scripts/theme-other.js
@@ -1,15 +1,18 @@
-var themeToggle = document.querySelector('.btn--theme');
+var themeToggle = document.querySelectorAll('.btn--theme');
 var body = document.querySelector('body');
-themeToggle.addEventListener('click', function () {
-  if (body.classList.contains('reverse')) {
-    primarycolor = '#333333';
-    secondarycolor = '#e7e7e7';
-    Cookies.set('theme', 'regular');
-    body.classList.remove('reverse');
-  } else {
-    primarycolor = '#e7e7e7';
-    secondarycolor = '#333333';
-    body.classList.add('reverse');
-    Cookies.set('theme', 'reverse');
-  }
-});
+foreach( toggle in themeToggle){
+  toggle.addEventListener('click', function () {
+    if (body.classList.contains('reverse')) {
+      primarycolor = '#333333';
+      secondarycolor = '#e7e7e7';
+      Cookies.set('theme', 'regular');
+      body.classList.remove('reverse');
+    } else {
+      primarycolor = '#e7e7e7';
+      secondarycolor = '#333333';
+      body.classList.add('reverse');
+      Cookies.set('theme', 'reverse');
+    }
+  });
+}
+
